54eb30b5c1f28cc5501dcd80004a86e1b0deb052
Marialaura Bancheri <maryban@hotmail.it>
2017-09-14 15:39:38 +0200

    The code was cleaned from old components not used anymore.

--- src/main/java/runoff/ETModel.java
+++
@@ -1,7 +0,0 @@
-package runoff;
-
-public interface ETModel {
-	
-	abstract public double ETcoefficient ();
-
-}
--- src/main/java/runoffOLD/ETModel.java
+++
@@ -1,7 +0,0 @@
-package runoffOLD;
-
-public interface ETModel {
-	
-	abstract public double ETcoefficient ();
-
-}
